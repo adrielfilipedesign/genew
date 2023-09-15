669dc61e6386566f9d22cdcd89afbe4063fd87e1
Adriel Filipe <88461833+adrielfilipedesign@users.noreply.github.com>
2023-09-15 11:45:31 -0400

    Primeiro commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# Gerador de Boletins online
+
+Link para acessar:
+https://adriel-filipe.github.io/genew/
---
+++ public/js/save_image.js
@@ -0,0 +1,5 @@
+function saveImage() {
+    //var canvas = document.getElementById("content");
+    //var img = canvas.toDataURL("image/png");
+    alert('funcionou');
+    }
